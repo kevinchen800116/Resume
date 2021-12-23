2d319b31e2ea7c56f929f33ec021d6701c8701af
unknown <kevin70852000@gmail.com>
2021-12-24 00:31:11 +0800

    修改內容2

--- config/index.js
+++ config/index.js
@@ -44,12 +44,11 @@ module.exports = {
 
   build: {
     // Template for index.html
-    // index: path.resolve(__dirname, '../dist/index.html'),
-    index: path.resolve(__dirname, '../docs/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    // index: path.resolve(__dirname, '../docs/index.html'),
 
     // Paths
-    // assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     // assetsPublicPath: '/',
     assetsPublicPath: '/Resume',
