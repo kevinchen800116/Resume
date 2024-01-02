787fd481885767279e3a08d68563ea418d76a605
unknown <kevin70852000@gmail.com>
2024-01-02 22:50:23 +0800

    update10

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -9,16 +9,7 @@
         preload="nona"
         :loop="true"
         :src="playSrc"></vue-core-video-player>
-      <!-- <video 
-        controls width="60%" muted autoplay preload loop
-        x5-video-player-fullscreen="true"
-        x5-playsinline
-        playsinline
-        webkit-playsinline
-        @click="playVideo">
-        <source src="require(@/media/sample.mp4)" type="video/mp4">
-        Your browser does not support the video tag.
-      </video> -->
+        <button @click="playVideo">播放视频</button>
     </div>
   </template>
   
@@ -30,11 +21,11 @@
         resolution: 720,
       }
     },
-    // methods: {
-    //   playVideo() {
-    //     this.$refs.video.play();
-    //   }
-    // }
+  methods: {
+    playVideo() {
+      this.$refs.videoPlayer.play();
+    },
+  },
   }
   </script>
   
