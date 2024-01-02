e765f8ae9062b43ebe7cd9afb84208a30ee3dc90
unknown <kevin70852000@gmail.com>
2024-01-02 22:07:32 +0800

    update10

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,20 +1,31 @@
 <template>
     <div>
-      <!-- <vue-core-video-player src="require(../assets/sample.mp4)"></vue-core-video-player> -->
-      <video 
-        controls width="60%"  muted playsinline 
+      <vue-core-video-player  
+        muted autoplay preload loop
         x5-video-player-fullscreen="true"
         x5-playsinline
-        webkit-playsinline 
+        playsinline
+        webkit-playsinline :src="playSrc"></vue-core-video-player>
+      <!-- <video 
+        controls width="60%" muted autoplay preload loop
+        x5-video-player-fullscreen="true"
+        x5-playsinline
+        playsinline
+        webkit-playsinline
         @click="playVideo">
         <source src="require(@/media/sample.mp4)" type="video/mp4">
         Your browser does not support the video tag.
-      </video>
+      </video> -->
     </div>
   </template>
   
   <script>
   export default {
+    data(){
+      return{
+        playSrc:require("@/assets/sample.mp4")
+      }
+    },
     methods: {
       playVideo() {
         this.$refs.video.play();
